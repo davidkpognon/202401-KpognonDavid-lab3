58558d72e119d72f8ddc841492a20f9b7037e55f
David <dskpognon20@students.desu.edu>
2023-11-30 19:10:12 -0500

    changes

--- lab3.c
+++ lab3.c
@@ -4,28 +4,34 @@
 #include "lab3.h"
 
 extern int** sudoku_board;
-int* worker_validation;
+int worker_validation;
 
-int** read_board_from_file(char* filename){
-    FILE *fp = NULL;
-    int** board = (int**)malloc(sizeof(int*) * ROW_SIZE);
-    
-    for (int row = 0; row < ROW_SIZE; row++) {
-        board[row] = (int*)malloc(sizeof(int) * COL_SIZE);
-    }
-
-    fp = fopen(filename, "r");
+//  read Sudoku board from file
+int** read_board_from_file(char* filename) {
+    // Open the file for reading
+    FILE *fp = fopen(filename, "r");
     if (fp == NULL) {
         perror("Error opening file");
         exit(EXIT_FAILURE);
     }
 
-    for (int d = 0; d < ROW_SIZE; d++) {
-        for (int r = 0; r < COL_SIZE; r++) {
-            fscanf(fp, "%d%*c", &board[d][r]);
+    // allocate memory for the 
+    sudoku_board = (int**)malloc(sizeof(int*) * ROW_SIZE);
+    for (int row = 0; row < ROW_SIZE; row++) {
+        sudoku_board[row] = (int*)malloc(sizeof(int) * COL_SIZE);
+        // read values from the file 
+        for (int col = 0; col < COL_SIZE; col++) {
+            // check if the expected number of values are read from the file
+            if (fscanf(fp, "%d,", &sudoku_board[row][col]) != 1) {
+                perror("Error reading from file");
+                exit(EXIT_FAILURE);
+            }
         }
-    }   
+    }
 
+    // Close the file after reading
     fclose(fp);
-    return board;
+    
+    return sudoku_board;
 }
+
